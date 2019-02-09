fb4a9c4e834b7016d4a0fb3ebafb7de2ad497be7
Thomas Maier <mjcross@hotmail.de>
2019-02-09 19:04:02 +0100

    Pipeline test

--- api/controller/main.js
+++ api/controller/main.js
@@ -2,6 +2,6 @@ exports.main = (req, res, next) => {
   return res.status(200).json({
     message: "Welcome Home!",
     author: "Thomas Maier",
-    timestamp: new Date().toISOString()
+    time: new Date().toISOString()
   });
 };
